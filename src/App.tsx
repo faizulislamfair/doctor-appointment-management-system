00070dcda1809517880ada18fd4e0b684d46f8b8
faizulislamfair <faizulislamfair@gmail.com>
2025-09-03 20:40:44 +0600

    feat: home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router"; 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import PatientDashboard from "./pages/patient/Dashboard";
 import PatientAppointments from "./pages/patient/Appointments";
 import DoctorDashboard from "./pages/doctor/Dashboard";
+
 
 const queryClient = new QueryClient();
 
@@ -14,12 +16,18 @@ export default function App() {
     <QueryClientProvider client={queryClient}>
       <Router>
         <Routes>
-          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path="/" element={<Navigate to="/home" replace />} />
+
+          <Route path="/home" element={<Home />} />
+
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+
           <Route path="/patient/dashboard" element={<PatientDashboard />} />
           <Route path="/patient/appointments" element={<PatientAppointments />} />
+
           <Route path="/doctor/dashboard" element={<DoctorDashboard />} />
+
           <Route path="*" element={<h1>404 Not Found</h1>} />
         </Routes>
       </Router>
---
+++ src/pages/Home.tsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router";
+import { Button } from "../../src/components/Button";
+
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50 p-6">
+      {/* Title */}
+      <h1 className="text-3xl md:text-4xl font-bold mb-8 text-center">
+        Doctor Appointment Management System
+      </h1>
+
+      {/* Buttons */}
+      <div className="flex gap-4">
+        <Link to="/login">
+          <Button className="px-6 py-3 text-lg rounded-lg">Login</Button>
+        </Link>
+        <Link to="/register">
+          <Button className="px-6 py-3 text-lg rounded-lg">
+            Register
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
+}
